b20a4aa9324d582652439ef8071e22abeca6b3ab
Urvesh <90395969+Urvessss@users.noreply.github.com>
2021-10-28 17:23:42 +0530

    commit

---
+++ src/Lab3/Exersize9.java
@@ -0,0 +1,20 @@
+package Lab3;
+
+
+	import java.time.*;
+	
+	
+	public class Exersize9 {	
+		
+		   public static void main(String[] args)
+		    {
+		        LocalDate pdate = LocalDate.of(1998,05,26);
+		        LocalDate now = LocalDate.now();
+		 
+		        Period diff = Period.between(pdate, now);
+		 
+		     System.out.printf("\nDifference is %d years, %d months and %d days old:", 
+		                    diff.getYears(), diff.getMonths(), diff.getDays());
+		  }
+		}
+
