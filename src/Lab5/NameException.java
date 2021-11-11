e2f38d1e66f3d0eb6b16b8b03a1c7e71cb5bcad3
Urvesh <90395969+Urvessss@users.noreply.github.com>
2021-11-11 17:19:05 +0530

    c

--- src/Lab5/NameException.java
+++ src/Lab5/NameException.java
@@ -2,5 +2,29 @@ package Lab5;
 
 public class NameException extends Exception
 {
-
+	NameException (String s)
+	{
+		super (s);
+	}
+	public static void validate(String s1,String s2)throws NameException
+	{
+		if(s1.isEmpty()||s2.isEmpty() )
+		{
+			throw new NameException("ENTER FIRST AND LAST NAME");
+		}
+		else
+		{
+			System.out.println("Your welcome");
+		}
+	}
+	public static void main(String[] args) {
+		String fname="";
+		String lname="";
+		try {
+			validate(fname,lname);
+		} catch (NameException e) {
+			System.out.println("Exception occured"+e.getMessage());
+			e.printStackTrace();
+		}
+	}
 }
